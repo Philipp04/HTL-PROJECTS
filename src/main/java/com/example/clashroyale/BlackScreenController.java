538fa65108064a530c693607581ebcb3f7c24108
Philipp04 <58029919+Philipp04@users.noreply.github.com>
2022-06-29 15:51:29 +0200

    gameplay Scene

---
+++ src/main/java/com/example/clashroyale/BlackScreenController.java
@@ -0,0 +1,23 @@
+package com.example.clashroyale;
+
+import javafx.fxml.FXML;
+import javafx.geometry.Insets;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.paint.Color;
+
+public class BlackScreenController {
+
+    @FXML
+    private AnchorPane background;
+
+    public void initialize(){
+        BackgroundFill background_fill = new BackgroundFill(Color.BLACK,
+                CornerRadii.EMPTY, Insets.EMPTY);
+
+        Background bg = new Background(background_fill);
+        background.setBackground(bg);
+    }
+}
